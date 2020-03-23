c630495a32ce1abd3eaaf0e9bb94b1c7481140f0
Carl Sutton <dogmatic69@gmail.com>
2020-03-23 22:09:30 +0100

    fix: use env correctly

--- docker/audit/trivy/files/work/entrypoint.sh
+++ docker/audit/trivy/files/work/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "Running trivy scan: [${INPUT_IMAGE}]"
+GITHUB_TOKEN=${INPUT_TOKEN} \
 trivy --no-progress --exit-code 1 \
-        --env GITHUB_TOKEN=${INPUT_TOKEN} \
 	-f json -o /output/trivy.json \
 	--ignorefile /.trivyignore \
 	--clear-cache \
-    "${INPUT_IMAGE}"
+        "${INPUT_IMAGE}"
