8f15bb5eca7fdf2b5256a6dda1684a3a2a0a7249
Carl Sutton (dogmatic69) <dogmatic69@gmail.com>
2020-04-07 23:43:35 +0200

    feat: Make target size configurable

--- file/lint/checkmake/files/work/entrypoint.sh
+++ file/lint/checkmake/files/work/entrypoint.sh
@@ -6,12 +6,16 @@ echo "Inputh Path: [${INPUT_PATH}]";
 
 #==============================================================================
 
+echo "
+[maxbodylength]
+maxBodyLength = ${INPUT_MAX_BODY_LENGHT:-5}" >> /tmp/checkmake.ini
+
 echo "Searching for Makefiles on [${INPUT_PATH}]"
 
 FAILED=0
 for file in $(find ${INPUT_PATH} -type f -name Makefile* -print); do
   echo "Linting: [${file}]"
-  /checkmake "${file}"
+  /checkmake --config=/tmp/checkmake.ini "${file}"
   FAILED=$(( ${FAILED} || $? ))
 done
 
