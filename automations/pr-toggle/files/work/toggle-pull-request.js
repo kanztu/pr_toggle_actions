b34e7dc615d94925cec249d159cb8119f54ee6a8
Carl Sutton (dogmatic69) <dogmatic69@gmail.com>
2020-04-10 22:49:55 +0200

    fix: get pr id when branch already exists
    Due to the plugin creating the PR not outputting a PR number
    when it exists, we need to use the github provided PR number
    when the workflow is run again (updated, reopened etc).

--- automations/pr-toggle/files/work/toggle-pull-request.js
+++ automations/pr-toggle/files/work/toggle-pull-request.js
@@ -1,14 +1,31 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const prId = () => {
+  if (process.env['PULL_REQUEST_NUMBER']) {
+    core.info(`PR # from automated PR action [${process.env['PULL_REQUEST_NUMBER']}]`);
+    return process.env['PULL_REQUEST_NUMBER'];
+  }
+
+  const result = /refs\/pull\/(\d+)\/merge/g.exec(process.env['GITHUB_REF']);
+  if (!result) {
+    return null;
+  }
+  const [, pullRequestId] = result;
+
+  core.inf(`PR # from existing PR [${pullRequestId}]`)
+  return pullRequestId;
+};
 
 module.exports = async () => {
-  const token = process.env["GITHUB_TOKEN"];
+  core.info(JSON.stringify(process.env))
+  const token = process.env['GITHUB_TOKEN'];
   if (!token) {
-    return core.setFailed("No token provided")
+    return core.setFailed('No token provided')
   }
-  const pr = process.env["PULL_REQUEST_NUMBER"];
+  const pr = prId();
   if (!pr) {
-    return core.setFailed("No PR number provided")
+    return core.setFailed('No PR number provided')
   }
 
   const client = new github.GitHub(token);
