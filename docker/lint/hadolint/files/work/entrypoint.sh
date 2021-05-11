d781256d2ab645764e33ff30831b9aa73d5ac082
Carl Sutton <dogmatic69@gmail.com>
2021-05-11 13:41:25 +0200

    fix: remove output

--- docker/lint/hadolint/files/work/entrypoint.sh
+++ docker/lint/hadolint/files/work/entrypoint.sh
@@ -2,16 +2,8 @@
 set -e
 
 SEARCH_PATH=$(realpath ${INPUT_PATH})
-OUTPUT=/output/hadolint.txt
 
 #==============================================================================
-function finish {
-  cat ${OUTPUT}
-}
-trap finish EXIT
-
-mkdir -p "$(dirname ${OUTPUT})"
-touch "${OUTPUT}"
 
 echo "Searching for Dockerfiles on [${SEARCH_PATH}]"
 
@@ -19,7 +11,7 @@ FAILED=0
 for file in $(find ${SEARCH_PATH} -type f -name Dockerfile*); do
   file=$(realpath ${file})
   echo "Scanning [${file}]"
-  docker run --rm -i hadolint/hadolint < ${file} >> ${OUTPUT}
+  docker run --rm -i hadolint/hadolint < ${file}
   FAILED=$(( ${FAILED} || $? ))
 done
 
